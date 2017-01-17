4ce2fd1666edea514a80a22df72aada5db4ad4d5
wangwindows <wangwindows@gmail.com>
2017-01-17 22:37:45 +0800

    month item

--- spider/spider/items.py
+++ spider/spider/items.py
@@ -8,8 +8,42 @@
 import scrapy
 
 
-class CitiesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class HistoryCitiesItem(scrapy.Item):
     city_name = scrapy.Field()
     city_url = scrapy.Field()
+
+
+class HistoryMonthItem(scrapy.Item):
+    city_id = scrapy.Field()
+    city_name = scrapy.Field()
+    hm_year = scrapy.Field()
+    hm_month = scrapy.Field()
+    hm_aqi = scrapy.Field()
+    hm_aqi_min = scrapy.Field()
+    hm_aqi_max = scrapy.Field()
+    hm_quality = scrapy.Field()
+    hm_pm25 = scrapy.Field()
+    hm_pm10 = scrapy.Field()
+    hm_so2 = scrapy.Field()
+    hm_co = scrapy.Field()
+    hm_no2 = scrapy.Field()
+    hm_o3 = scrapy.Field()
+    hm_rank = scrapy.Field()
+    hm_day_url = scrapy.Field()
+
+
+class HistoryDayItem(scrapy.Item):
+    city_id = scrapy.Field()
+    city_name = scrapy.Field()
+    hd_date = scrapy.Field()
+    hd_aqi = scrapy.Field()
+    hd_aqi_min = scrapy.Field()
+    hd_aqi_max = scrapy.Field()
+    hd_quality = scrapy.Field()
+    hd_pm25 = scrapy.Field()
+    hd_pm10 = scrapy.Field()
+    hd_so2 = scrapy.Field()
+    hd_co = scrapy.Field()
+    hd_no2 = scrapy.Field()
+    hd_o3 = scrapy.Field()
+    hd_rank = scrapy.Field()
--- spider/spider/spiders/aqistudy.py
+++ spider/spider/spiders/aqistudy.py
@@ -1,13 +1,17 @@
+# -*- coding: utf-8 -*-
 import scrapy
 from spider.items import CitiesItem
 
 
-class CitiesSpider(scrapy.Spider):
-    name = "cities"
+class HistorySpider(scrapy.Spider):
+    name = "history"
     allowed_domains = ["aqistudy.cn"]
     start_urls = [
         "https://www.aqistudy.cn/historydata/index.php"
     ]
+
+    def __init__(self, *args, **kwargs):
+        super(HistorySpider, self).__init__(*args, **kwargs)
 
     def parse(self, response):
 
@@ -23,19 +27,46 @@ class CitiesSpider(scrapy.Spider):
 
             print "cities: %s, url: %s " % (city_name, city_url)
 
+            yield scrapy.Request(url=city_url,
+                                 meta={'city_name': city_name},
+                                 callback=self.parse_month,
+                                 priority=20)
+            """
             item = CitiesItem()
             item['city_name'] = city_name
             item['city_url'] = city_url
             yield item
+            """
 
-#
-# class MonthSpider(scrapy.Spider):
-#
-#    def __init__(self, *args, **kwargs):
-#        super(MonthSpider, self).__init__(*args, **kwargs)
-#
-#        # self.start_urls = ['http://www.example.com/categories/%s' % category]
-#
-#    def parse(self, response):
-#        pass
-#
+    def parse_month(self, response):
+        month_list = response.xpath('//table//tr[position()>1]')
+        for month in month_list:
+            # 处理日期
+            month_a = month.xpath('//a')
+            hm_day_url = month_a.xpath('@href').extract_first()
+            month_text = month_a.xpath('text()').extract_first()
+            month_text = month_text.split(u'-')
+            hm_year = month_text[0]
+            hm_month = month_text[1]
+
+            hm_aqi = month.xpath('//td[2]/text()').extract_first()
+
+            aqi_avg = month.xpath('//td[3]/text()').extract_first()
+            aqi_avg = aqi_avg.split(u'~')
+            hm_aqi_min = aqi_avg[0]
+            hm_aqi_max = aqi_avg[1]
+
+            hm_quality = month.xpath('//td[4]/text()').extract_first()
+            hm_pm25 = month.xpath('//td[5]/text()').extract_first()
+            hm_pm10 = month.xpath('//td[6]/text()').extract_first()
+            hm_so2 = month.xpath('//td[7]/text()').extract_first()
+            hm_co = month.xpath('//td[8]/text()').extract_first()
+            hm_no2 = month.xpath('//td[9]/text()').extract_first()
+            hm_o3 = month.xpath('//td[10]/text()').extract_first()
+
+            hm_rank = month.xpath('//td[11]/text()').extract_first()
+
+
+
+
+
