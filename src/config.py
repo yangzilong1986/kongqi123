6d0bdc47d06081d77c561d9a3fc4e0f3700b861d
wangwindows <wangwindows@gmail.com>
2017-01-26 23:35:29 +0800

    load data

--- src/config.py
+++ src/config.py
@@ -1,11 +1,25 @@
+# -*- coding: utf-8 -*-
+
+MODE = 'dev'
+
 # 数据库 MySQL
-DB_MYSQL = {
-    'host': '10.0.0.1',
-    'user': 'root',
-    'passwd': 'ayumihamasaki',
-    'port': 3306,
-    'db': 'pm25'
-}
+if MODE == 'dev':
+    DB_MYSQL = {
+        'host': '127.0.0.1',
+        'user': 'root',
+        'passwd': '1987527',
+        'port': 3306,
+        'db': 'pm25'
+    }
+else:
+    DB_MYSQL = {
+        'host': '10.0.0.1',
+        'user': 'root',
+        'passwd': 'ayumihamasaki',
+        'port': 3306,
+        'db': 'pm25'
+    }
+
 SQLALCHEMY_DATABASE_URI_MYSQL = \
     'mysql+mysqldb://%s:%s@%s:%s/%s?charset=utf8' % \
     (DB_MYSQL['user'], DB_MYSQL['passwd'], DB_MYSQL['host'], DB_MYSQL['port'], DB_MYSQL['db'])
--- src/demo.py
+++ src/demo.py
@@ -1,7 +1,7 @@
+# -*- coding: utf-8 -*-
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from spider.model import HistoryCity, HistoryDay, HistoryMonth
 from config import *
 import pandas as pd
 import numpy as np
@@ -27,9 +27,27 @@ class Demo(object):
 
     def load_daily_city_data(self, city_id, start_date, end_date):
         row_sql = "select * from history_day " \
-                  "where city_id = :city_id and hd_date >= :start_dat and hd_date <= :end_date"
+                  "where city_id = :city_id and hd_date >= :start_date and hd_date <= :end_date"
         row_data = {"city_id": city_id, "start_date": start_date, "end_date": end_date}
         data = self.session.execute(row_sql, row_data).fetchall()
         return data
 
+    def test_simple_tree(self, city_name, start_date, end_date):
+        city_info = self.get_city_info_by_name(city_name)
+        if not city_info:
+            print '不存在的城市: %s' % (city_name, )
+            return
 
+        city_id = city_info['city_id']
+        data = self.load_daily_city_data(city_id, start_date, end_date)
+        if not data:
+            print '城市没有数据: %s' % (city_name, )
+            return
+
+        df = pd.DataFrame(data)
+        df.columns = data.keys()
+
+
+if __name__ == '__main__':
+    demo = Demo()
+    demo.test_simple_tree(u'上海', '2016-01-01', '2016-12-31')
