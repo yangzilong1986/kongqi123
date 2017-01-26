1a3ab65da0724247ede108e4507f7f3f24c907ad
wangwindows <wangrui@wealink.com>
2017-01-26 12:11:14 +0800

    demo

--- src/demo.py
+++ src/demo.py
@@ -4,5 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from spider.model import HistoryCity, HistoryDay, HistoryMonth
 from config import *
 
-def load_data_from():
-    pass
+
+class Demo(object):
+    def __init__(self):
+        self.engine = create_engine(SQLALCHEMY_DATABASE_URI_MYSQL, pool_size=SQLALCHEMY_POOL_SIZE)
+        self.db_session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+
+    def load_data_from_mysql(self):
+        pass
