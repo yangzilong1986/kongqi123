b0ccf46f902abd93a314e0efb7d55514d1fb2aab
wangwindows <wangwindows@gmail.com>
2017-01-30 12:16:32 +0800

    tree

--- src/demo.py
+++ src/demo.py
@@ -3,6 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import *
+from collections import defaultdict
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import cross_val_score
 import pandas as pd
@@ -59,15 +60,62 @@ class Demo(object):
         df['hd_no2'] = df['hd_no2'].astype(np.double)
         df['hd_o3'] = df['hd_o3'].astype(np.double)
 
-        print df
-        print df.dtypes
+        df['level_1'] = df['hd_pm25'] <= 35
+        df['level_2'] = df['hd_pm25'] > 35 & df['hd_pm25'] <= 75
+
+        """
+        df['level_3'] = 75 > df['hd_pm25'] <= 115
+        df['level_4'] = 115 > df['hd_pm25'] <= 150
+        df['level_5'] = 150 > df['hd_pm25'] <= 250
+        df['level_6'] = df['hd_pm25'] > 250
+        """
+
+        print df['hd_pm25'], df['level_1']
+            # , df['level_2'], df['level_3'], df['level_4'], df['level_5'], df['level_6']
+
 
         '''
+        for index, row in df.iterrows():
+            row['level'] = 1
+            if row['hd_pm25'] > 35:
+                row['level'] = 2
+            elif row['hd_pm25'] > 75:
+                row['level'] = 3
+            elif row['hd_pm25'] > 115:
+                row['level'] = 4
+            elif row['hd_pm25'] > 150:
+                row['level'] = 4
+            elif row['hd_pm25'] > 250:
+                row['level'] = 5
+            elif row['hd_pm25'] > 500:
+                row['level'] = 6
+
+        print df['hd_pm25']
+        print df.dtypes
+
         clf = DecisionTreeClassifier(random_state=14)
         x_pm25 = df['hd_pm25'].values
         pm25 = cross_val_score(clf, pm25, y_true)
         '''
 
 if __name__ == '__main__':
+    """
+    PM2.5 中国标准
+    0-35    一级 优
+    35-75   二级 良
+    75-115  三级 轻度污染
+    115-150 四级 中度污染
+    150-250 五级 重度污染
+    250-500 六级 严重污染
+
+    PM2.5 美国标题
+    0-12    一级 优
+    12-35   二级 良
+    35-55   三级 轻度污染
+    55-150  四级 中度污染
+    150-250 五级 重度污染
+    250-500 六级 严重污染
+    """
+
     demo = Demo()
     demo.test_simple_tree(u'上海', '2016-01-01', '2016-12-31')
