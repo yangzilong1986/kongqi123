a0d0e9f23d7dfb6a3f4c8a2d2467c2ad41ca2135
wangwindows <wangwindows@gmail.com>
2017-02-02 10:11:06 +0800

    test model

--- src/demo.py
+++ src/demo.py
@@ -5,7 +5,21 @@ from sqlalchemy.orm import sessionmaker
 from config import *
 from collections import defaultdict
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.cross_validation import cross_val_score
+
+# 数据切分为train和test
+# from sklearn.cross_validation import cross_val_score, train_test_split
+# Use:func:`sklearn.model_selection.cross_val_score` instead.
+# Use:func:`sklearn.model_selection.train_test_split` instead.
+from sklearn.model_selection import train_test_split
+
+# 指标来评估模型
+from sklearn import metrics
+
+# 交叉验证来评估模型
+# from sklearn import cross_validation
+# Use :func:`sklearn.model_selection.cross_val_score` instead.
+from sklearn.model_selection import cross_val_score
+
 import pandas as pd
 import numpy as np
 
@@ -61,18 +75,79 @@ class Demo(object):
         df['hd_o3'] = df['hd_o3'].astype(np.double)
 
         df['level_1'] = df['hd_pm25'] <= 35
-        df['level_2'] = df['hd_pm25'] > 35 & df['hd_pm25'] <= 75
-
-        """
-        df['level_3'] = 75 > df['hd_pm25'] <= 115
-        df['level_4'] = 115 > df['hd_pm25'] <= 150
-        df['level_5'] = 150 > df['hd_pm25'] <= 250
+        df['level_2'] = (df['hd_pm25'] > 35) & (df['hd_pm25'] <= 75)
+        df['level_3'] = (df['hd_pm25'] > 75) & (df['hd_pm25'] <= 115)
+        df['level_4'] = (df['hd_pm25'] > 115) & (df['hd_pm25'] <= 150)
+        df['level_5'] = (df['hd_pm25'] > 150) & (df['hd_pm25'] <= 250)
         df['level_6'] = df['hd_pm25'] > 250
+
+        for index, row in df.iterrows():
+            if row['hd_pm25'] <= 35:
+                row['level'] = 1
+            elif 35 > row['hd_pm25'] <= 75:
+                row['level'] = 2
+            elif 75 > row['hd_pm25'] <= 115:
+                row['level'] = 3
+            elif 115 > row['hd_pm25'] <= 150:
+                row['level'] = 4
+            elif 150 > row['hd_pm25'] <= 250:
+                row['level'] = 5
+            elif row['hd_pm25'] > 250:
+                row['level'] = 6
+
+            # df[index]['level'] = row['level']
+
+            print "%s\t PM.5: %s,\t %s\t %s,\t %s,\t %s,\t %s,\t %s,\t %s" % (
+                index,
+                row['hd_pm25'],
+                row['level'],
+                row['level_1'],
+                row['level_2'],
+                row['level_3'],
+                row['level_4'],
+                row['level_5'],
+                row['level_6'],
+            )
+        print df.dtypes
+
+        subdf = df[['pclass', 'sex', 'age']]
+        y = df.survived
+
+        # sklearn中的Imputer也可以
+        age = subdf['age'].fillna(value=subdf.age.mean())
+        # sklearn OneHotEncoder也可以
+        pclass = pd.get_dummies(subdf['pclass'], prefix='pclass')
+        sex = (subdf['sex'] == 'male').astype('int')
+
+        x = pd.concat([pclass, age, sex], axis=1)
+        x.head()
+
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=14)
+        clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=3, min_samples_leaf=5)
+        clf = clf.fit(x_train, y_train)
+        print "准确率为：{:.2f}".format(clf.score(x_test, y_test))
+
+        # 使用更多指标来评估模型
         """
+        def measure_performance(X, y, clf, show_accuracy=True,
+                                show_classification_report=True,
+                                show_confusion_matrix=True):
+        """
+        # measure_performance(x_test, y_test, clf, show_classification_report=True, show_confusion_matrix=True)
+        x = x_test
+        y = y_test
+        y_pred = clf.predict(x)
+        print "Accuracy:{0:.3f}".format(metrics.accuracy_score(y, y_pred)), "\n"
 
-        print df['hd_pm25'], df['level_1']
-            # , df['level_2'], df['level_3'], df['level_4'], df['level_5'], df['level_6']
+        print "Classification report"
+        print metrics.classification_report(y, y_pred), "\n"
 
+        print "Confusion matrix"
+        print metrics.confusion_matrix(y, y_pred), "\n"
+
+        # 交叉验证来评估模型
+        scores1 = cross_validation.cross_val_score(clf, X, y, cv=10)
+        print scores1
 
         '''
         for index, row in df.iterrows():
@@ -97,6 +172,7 @@ class Demo(object):
         x_pm25 = df['hd_pm25'].values
         pm25 = cross_val_score(clf, pm25, y_true)
         '''
+
 
 if __name__ == '__main__':
     """
