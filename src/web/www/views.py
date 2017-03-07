175d5c99a814d0372f4df81d24b5d42668c2f28a
wangwindows <wangwindows@gmail.com>
2017-03-07 21:39:39 +0800

    report

--- src/web/www/views.py
+++ src/web/www/views.py
@@ -325,11 +325,18 @@ def report_weather_trend():
     weather_all = weather_client.all_day(condition)
     weather_am_types = weather_client.total_types(condition, 'weather_am')
     weather_pm_types = weather_client.total_types(condition, 'weather_pm')
-    print weather_am_types, type(weather_am_types), type(weather_all)
+    weather_am_wind_types = weather_client.total_types(condition, 'weather_am_wind_type')
+    weather_pm_wind_types = weather_client.total_types(condition, 'weather_pm_wind_type')
+    weather_am_level_types = weather_client.total_types(condition, 'weather_am_wind_level')
+    weather_pm_level_types = weather_client.total_types(condition, 'weather_pm_wind_level')
 
     for weather in weather_all:
         weather['weather_am_index'] = weather_am_types.index(weather['weather_am'])
         weather['weather_pm_index'] = weather_pm_types.index(weather['weather_pm'])
+        weather['weather_am_wind_index'] = weather_am_wind_types.index(weather['weather_am_wind_type'])
+        weather['weather_pm_wind_index'] = weather_pm_wind_types.index(weather['weather_pm_wind_type'])
+        weather['weather_am_level_index'] = weather_am_level_types.index(weather['weather_am_wind_level'])
+        weather['weather_pm_level_index'] = weather_pm_level_types.index(weather['weather_pm_wind_level'])
 
     data = dict()
     data['current_page'] = 'report'
